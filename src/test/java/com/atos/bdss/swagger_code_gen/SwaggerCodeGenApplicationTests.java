0f17744e5a7770e633e8750fd3b82e2c8a6461d7
Emmanuel Gillet <emmanuel.gillet@atos.net>
2020-01-08 11:18:14 +0100

    First Commit

---
+++ src/test/java/com/atos/bdss/swagger_code_gen/SwaggerCodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.bdss.swagger_code_gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerCodeGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
